09f26f7b277ef7fed8c9b7993b5b82e200bfed49
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-07-28 23:29:52 +0200

    Initial commit

---
+++ src/main/java/eu/pollux28/savemypiglin/SaveMyPiglin.java
@@ -0,0 +1,13 @@
+package eu.pollux28.savemypiglin;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class SaveMyPiglin implements ModInitializer {
+    public static final String VERSION = "1.0-SNAPSHOT";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
